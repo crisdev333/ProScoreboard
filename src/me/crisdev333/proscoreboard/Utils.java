37ff4e7c661b00b62f12ce98c6f8b14ee325e2d0
crisdev333 <crisdev333@gmail.com>
2017-07-05 13:35:12 +0200

    Small change + update to version 1.1.2

--- src/me/crisdev333/proscoreboard/Utils.java
+++ src/me/crisdev333/proscoreboard/Utils.java
@@ -4,19 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 public class Utils {
-	
+
 	public static final List<String> EMPTY_LIST = new ArrayList<>();
-	
+
 	public static List<Player> getOnlinePlayers() {
 		ArrayList<Player> list = new ArrayList<>();
-		for(World world : Bukkit.getWorlds()) {
-			for(Player player: world.getPlayers()) {
-				list.add(player);
-			}
+		for(Player player : Bukkit.getOnlinePlayers()) {
+			list.add(player);
 		}
 		return list;
 	}
